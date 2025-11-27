cdeab0c03839aa1847735d33cadb5dc3753ec204
Sazid <sazid479@gmail.com>
2025-11-27 16:15:12 +0600

    Fixed bugs

--- software/main.py
+++ software/main.py
@@ -1,11 +1,11 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow
-from ui_main import Ui_MainWindow
+from ui_main import Ui_AdminDashboard
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.ui = Ui_MainWindow()
+        self.ui = Ui_AdminDashboard()
         self.ui.setupUi(self)
 
 
